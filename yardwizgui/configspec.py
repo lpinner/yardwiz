9a1025d9d5738d2a00744f96fcf23081357b9561
pinner.luke <pinner.luke@0c20e359-ab2e-c4a3-e67c-01af7e14c4b1>
2011-05-04 11:21:46 +0000

    Don't make quick listing the only listing option.

--- yardwizgui/configspec.py
+++ yardwizgui/configspec.py
@@ -3,6 +3,7 @@ postdownloadtooltip='Usage: somecommand [%F] [%D]\n\tOptional %F and/or %D param
 dateformattooltip='The date format string is as per the python date/time format codes listed at:\nhttp://docs.python.org/library/time.html#time.strftime'
 soundstooltip='Sound files must be 8 bit mono WAV format with no metadata tags.'
 deletetooltip='Confirm that you really wish to delete a recording from your Beyonwiz.'
+quicklistingtooltip='Note: enabling quick listing will show any files that have been deleted from your Wiz and will allow you to attempt to delete recordings that are locked, which will fail.'
 configspec={
     'Settings':{
         'device':['Device', 'str', devicetooltip],
@@ -10,7 +11,8 @@ configspec={
         'postdownloadcommand':['Post download command', 'str', postdownloadtooltip],
         'display_dateformat':['Date format for display','str', dateformattooltip],
         'filename_dateformat':['Date format for filenames', 'str', dateformattooltip],
-        'confirmdelete':['Confirm delete', 'bool']
+        'confirmdelete':['Confirm delete', 'bool'],
+        'quicklisting':['Enable quick listing', 'bool',quicklistingtooltip ]
         },
     'Sounds':{ #This will need to be rewritten if we add more sounds...
         'playsounds':['Play sound after download', 'bool'],
