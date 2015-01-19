d8347c27eaf5b41a91eaf6af6f8b073b738fe367
lukepinnerau@gmail.com <lukepinnerau@gmail.com@0c20e359-ab2e-c4a3-e67c-01af7e14c4b1>
2015-01-19 19:48:41 +0000

    Update copyright Bump version

--- yardwizgui/configspec.py
+++ yardwizgui/configspec.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Luke Pinner
+# Copyright (c) 2015 Luke Pinner
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
