1cc9fcb30245e640f171b7a48e29f6e6a659aa68
lpinner <lukepinnerau@gmail.com>
2015-05-30 09:11:50 +1000

    Fix paths to new readme and releasenotes files

--- setup.py
+++ setup.py
@@ -146,9 +146,9 @@ elif 'darwin' in sys.platform and 'py2app' in sys.argv:
     import glob,stat
 
     APP = [ 'YARDWiz.py']
-    DATA_FILES = [('',['README']),
+    DATA_FILES = [('',['README.md']),
                   ('',['LICENSE']),
-                  ('',['RELEASE']),
+                  ('',['ReleaseNotes.md']),
                   ('',['VERSION'])]
     OPTIONS = {'extension': '.app', 'packages': 'yardwizgui',
                'iconfile': 'yardwizgui/icons/yardwiz.icns',
@@ -269,9 +269,9 @@ elif 'py2exe' in sys.argv:
                             }
                         }
     setupargs['data_files']=[('',['getwizpnp.exe']),
-                             ('',['README']),
+                             ('',['README.md']),
                              ('',['LICENSE']),
-                             ('',['RELEASE']),
+                             ('',['ReleaseNotes.md']),
                              ('',['VERSION']),
                              ('',['Microsoft.VC90.CRT.manifest']),
                              ('',glob.glob('msvc*90.dll')),
